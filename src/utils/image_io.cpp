a55859fbbe01b6a1055bba06e30fdba14fb92bfe
Alba NR <alba.navarro.ros@gmail.com>
2021-12-22 17:04:05 +0100

    Correctly flip images vertically to match OpenGL convention
    - (0,0) in normalised texture coords in  OpenGL refers to the bottom left corner
    - (0,0) for the images loaded by stbi refers to the top left corner => must flip image vertically to match OpenGL convention

--- src/utils/image_io.cpp
+++ src/utils/image_io.cpp
@@ -7,6 +7,8 @@ string TEXTURES_FOLDER_PATH = PROJECT_SOURCE_DIR "/resources/textures/";
 
 unsigned char *ImageIO::loadImage(string filename, int &width, int &height, int &num_channels)
 {
+	stbi_set_flip_vertically_on_load(true);
+
 	string filepath = TEXTURES_FOLDER_PATH + filename;
 	
 	unsigned char *img_data = stbi_load(filepath.c_str(), &width, &height, &num_channels, 0);
